14f41e0a794cd31d79d77d1a8c060b08fcf732eb
Erik P <erikvip@gmail.com>
2024-11-20 21:52:35 -0800

    Resync

--- hourly.sh
+++ hourly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export _WU_OPT_DECIMAL=1;
 cat data/v3-wx-forecast-hourly-15day.ca-post-mountain.json  | \
 	jq --arg opts "{}" -r -f lib/forecast-hourly.js
   #jq --arg opts "{}" -r -f tpl/forecast-hourly.js
